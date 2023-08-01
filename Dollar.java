bf265ea36f8a2796409e92ff5bc767c882531cca
unknown <eduarda.elger@unioeste.br>
2023-08-01 12:07:40 -0300

    Passo 4

--- Dollar.java
+++ Dollar.java
@@ -1,7 +1,9 @@
 class dollar extends money {
+   
    dollar(int amount, String currency){
       super(amount, currency);
     }			
+
     money times(int multiplier) {
       return money.dollars(amount * multiplier);
     }
